3b025400714580d0a630d08fd7da0a7023bcddd1
josue-desarrollos <rey_josue_xd@hotmail.com>
2021-02-05 23:50:49 -0500

    first commit

---
+++ Playground01.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var V1 = "esto es swift"
+let c1 = 4
+var cadena:String = String()
+cadena = "hola mundo" + V1
+print("el contenido es: \(cadena)")
+print("el valor de constante es: \(c1)")
+	
