c5fb0151239e4a365aa78026b32947d742252bd1
josue-desarrollos <rey_josue_xd@hotmail.com>
2021-03-25 00:40:48 -0400

    actividad 8 de iOS

---
+++ Actividad3.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+var numero = 4
+let decimal:Float = 5.7
+var dato1 = "Hello, playground"
+let dato2:String = "hola, playground"
+print("su cambio es \(decimal)")
+var error = ("no sirve", 404)
+var num:Array<Int> = Array<Int>()
+num.append(1)
+num.append(2)
+num.append(3)
+num.append(4)
+num.append(5)
+num.append(6)
+num.append(7)
+num.append(8)
+num.append(9)
+num.append(10)
+var DiasSemana:Dictionary<String, Int> = Dictionary<String, Int>()
+DiasSemana = ["LUN" : 1]
+DiasSemana = ["MAR" : 2]
+DiasSemana = ["MIE" : 3]
+DiasSemana = ["JUE" : 4]
+DiasSemana = ["VIE" : 5]
+DiasSemana = ["SAB" : 6]
+DiasSemana = ["DOM" : 7]
---
+++ acti8/acti8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  acti8
+//
+//  Created by user182861 on 3/25/21.
+//  Copyright © 2021 tecmilenio.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
