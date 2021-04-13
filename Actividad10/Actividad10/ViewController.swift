97ecf393e371bc5025c5571ccfab80fd58a2d6e0
josue-desarrollos <71675711+josue-desarrollos@users.noreply.github.com>
2021-04-13 01:14:46 -0500

    actividad 10
    adjunto actividad 10

---
+++ Actividad10/Actividad10/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Actividad10
+//
+//  Created by user182861 on 4/13/21.
+//  Copyright © 2021 tecmilenio.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
