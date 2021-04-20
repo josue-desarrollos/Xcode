098476808782dc64135405aba4429f543c6c6e94
josue-desarrollos <71675711+josue-desarrollos@users.noreply.github.com>
2021-04-20 18:08:45 -0500

    Add files via upload
    adjunto evidencia 2

---
+++ avance2/avance2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  avance2
+//
+//  Created by user182861 on 4/19/21.
+//  Copyright © 2021 tecmilenio.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
